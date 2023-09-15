95a1445c73d1ab036894d220c611354880bf24a4
kyaruwo <75692623+kyaruwo@users.noreply.github.com>
2023-09-15 19:49:46 +0800

    borgar is pain

--- js/index.js
+++ js/index.js
@@ -1,13 +1,16 @@
 function change_theme() {
     const rc = document.getElementById("root").classList
     const tbimg = document.getElementById("theme-btn-img")
+
     if (rc.contains("theme-dark")) {
-        tbimg.src = "assets/theme-dark.png";
-        rc.remove("theme-dark")
-        rc.add("theme-light")
+        tbimg.src = "assets/theme-todark.png"
     } else {
-        tbimg.src = "assets/theme-light.png";
-        rc.remove("theme-light")
-        rc.add("theme-dark")
+        tbimg.src = "assets/theme-tolight.png"
     }
+    rc.toggle("theme-dark")
+}
+
+function hide_borgar() {
+    document.getElementById("nav-links").classList.toggle("hide-borgar")
+    document.getElementById("hamborgar-bg").classList.toggle("show-hamborgar-bg")
 }
